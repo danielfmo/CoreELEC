702da0afbd2be22253435cb6df0fb87dc56e2b58
danielfmo <danielfilipe.mo@gmail.com>
2018-05-03 22:11:51 +0200

    first commit, non functional

--- packages/addons/script/moonlight/package.mk
+++ packages/addons/script/moonlight/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="moonlight"
-PKG_VERSION="4d94439"
-PKG_SHA256="5190f9c3a0fd17c7c8f0de8c2509f4749a2f399b7dc4d1402dd55c6f351260b2"
-PKG_VERSION_NUMBER="2.2.2"
-PKG_REV="109"
+PKG_VERSION="29f4511"
+PKG_SHA256="7bb7ed08345fde16a8aefb35ed6fca898ff83f0f45d1c2850b17ee7f1a7e84fc"
+PKG_VERSION_NUMBER="2.4.6"
+PKG_REV="111"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dead/script.moonlight"
@@ -40,7 +40,7 @@ PKG_ADDON_PROVIDES="executable"
 post_unpack() {
   # don't use the files from the script
   rm $PKG_BUILD/script.moonlight/icon.png
-  rm $PKG_BUILD/script.moonlight/changelog.txt
+  #rm $PKG_BUILD/script.moonlight/changelog.txt
 }
 
 addon() {
@@ -51,14 +51,18 @@ addon() {
     cp $PKG_DIR/changelog.txt $ADDON_BUILD/$PKG_ADDON_ID
 
     # let's use our addon.xml instead
-    rm $ADDON_BUILD/$PKG_ADDON_ID/addon.xml
+    #rm $ADDON_BUILD/$PKG_ADDON_ID/addon.xml
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/moonlight $ADDON_BUILD/$PKG_ADDON_ID/bin
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
     cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libgamestream.so.2.4.6 $ADDON_BUILD/$PKG_ADDON_ID/lib
+    #cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libgamestream.so.4 $ADDON_BUILD/$PKG_ADDON_ID/lib
+    #cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libgamestream.so $ADDON_BUILD/$PKG_ADDON_ID/lib
     cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libmoonlight-common.so.2.4.6 $ADDON_BUILD/$PKG_ADDON_ID/lib
+    #cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libmoonlight-common.so.4 $ADDON_BUILD/$PKG_ADDON_ID/lib
+    #cp $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libgamestream/libmoonlight-common.so $ADDON_BUILD/$PKG_ADDON_ID/lib
 
     if [ "$KODIPLAYER_DRIVER" = "bcm2835-driver" ]; then
       cp -P $(get_build_dir moonlight-embedded)/.$TARGET_NAME/libmoonlight-pi.so $ADDON_BUILD/$PKG_ADDON_ID/lib
